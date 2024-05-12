71c7e3ff1212a329b0769ae49d6357747d1abd72
AlexTkachuk1 <Lyucifer97@list.ru>
2024-05-12 23:18:46 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use bevy::prelude::*;
+use bevy::window::close_on_esc;
+
+const WW: f32 = 1200.;
+const WH: f32 = 700.;
+const SPRITE_SHEET_PATH: &str = "assets.png";
+const SPRITE_SCALE_FACTOR: f32 = 3.0;
+const TILE_W: usize = 16;
+const TILE_H: usize = 16;
+const SPRITE_SHEET_W: usize = 4;
+const SPRITE_SHEET_H: usize = 4;
+const BG_COLOR: (u8, u8, u8) = (197, 204, 184);
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resizable: true,
+                        focused: true,
+                        resolution: (WW, WH).into(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .insert_resource(ClearColor(Color::rgb_u8(BG_COLOR.0, BG_COLOR.1, BG_COLOR.2)))
+        .insert_resource(Msaa::Off)
+        .add_systems(Startup, (setup_camera, spawn_plaer))
+        .add_systems(Update, close_on_esc)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_plaer(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load(SPRITE_SHEET_PATH);
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(TILE_W as f32, TILE_H as f32), SPRITE_SHEET_W, SPRITE_SHEET_H, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    commands.spawn((
+        SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 0,
+            },
+            transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+            ..default()
+        },
+    ));
+}
